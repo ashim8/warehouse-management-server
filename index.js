4f1a6a043c80d06b81bce4bd7f03883634c76854
ashim8 <ashimshil54321@gmail.com>
2022-05-21 02:33:45 +0600

    get furniture data

--- index.js
+++ index.js
@@ -12,11 +12,20 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rnpem.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
     try{
+        await client.connect();
+        const furnitureCollection = client.db('warehouseProject').collection('product');
 
+        app.get('/product', async(req, res)=>{
+            const query = {};
+            const cursor = furnitureCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        })
     }
     finally{
         
