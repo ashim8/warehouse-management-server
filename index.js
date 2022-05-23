4a217b02dea487f4af028c54b544924c4fbf0893
ashim8 <ashimshil54321@gmail.com>
2022-05-24 04:18:20 +0600

    post api

--- index.js
+++ index.js
@@ -52,6 +52,21 @@ async function run(){
             const products = await cursor.toArray();
             res.send(products)
         });
+        app.get('/product/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await furnitureCollection.findOne(query);
+            res.send(product);
+        })
+
+        //POST
+        app.post('/product', async(req,res)=>{
+            const newProduct = req.body;
+            const result = await furnitureCollection.insertOne(newProduct);
+            res.send(result);
+        });
+
+       
     
     }
     finally{
